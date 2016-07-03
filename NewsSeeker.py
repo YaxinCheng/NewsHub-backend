e943ca07c16fa9ea115ce00cf46c0419701b771e
WatashiJ <yx299612@dal.ca>
2016-07-03 14:08:54 -0300

    Support Normal News

--- NewsSeeker.py
+++ NewsSeeker.py
@@ -7,6 +7,8 @@ class NewsSeeker:
 
 	headlinePatterns = {'metro': '<div class="top-story"(.*?|\s)*?<\/div>',
 						'chronicle': '<div class="text">(\s|.)*?<h2>.*?<\/h2>'}
+	normalPatterns = {'metro': '<li class="(no\-thumb|top\-story float\-clear)"(.|\s)*?<\/li>', 
+						'chronicle': '<div class="views-field-title cufon">(.|\s)*?<a href=".*?">.*?<\/a>(.|\s)*?<\/div>'}
 
 	def __init__(self, url, source):
 		self.url = url
@@ -15,8 +17,10 @@ class NewsSeeker:
 	def process(self):
 		self.data = self.__webInfo()
 		result = self.__headlines()
-		print("Count " + str(len(result)))
-		print(result[0])
+		normal = self.__normalNews()
+		print("Headline Count " + str(len(result)))
+		print("Normal Count " + str(len(normal)))
+		print(normal[0])
 
 	def __webInfo(self):
 		return urllib.request.urlopen(self.url).read().decode('UTF-8')
@@ -30,6 +34,11 @@ class NewsSeeker:
 		else:
 			return info.group()
 
+	def __findAllInfo(self, patternGroup):
+		pattern = patternGroup[self.source]
+		pattern = re.compile(pattern)
+		return re.finditer(pattern, self.data)
+
 	def __headlines(self):
 		if self.url is None or self.source is None:
 			raise ValueError
@@ -40,7 +49,9 @@ class NewsSeeker:
 			img = soup.img['data-original']
 			title = soup.h1.a.text
 			subtitle = soup.h2.text.strip()
-			return [News(title = title, url = url, source = self.source, content = subtitle, img = img)]
+			subHeadlines = self.__processMetroSubHeadlines()
+			subHeadlines.insert(0, News(title = title, url = url, source = self.source, content = subtitle, img = img))
+			return subHeadlines
 		elif self.source == 'chronicle':
 			headlinePattern = re.compile(self.headlinePatterns[self.source])
 			result = re.finditer(headlinePattern, self.data)
@@ -62,9 +73,46 @@ class NewsSeeker:
 				headlines.append(News(title = titles[index], url = urls[index], source = self.source, content = contents[index], img = imgs[index] if len(imgs) > index else ''))
 			return headlines
 
+	def __normalNews(self):
+		if self.url is None or self.source is None:
+			raise ValueError
+		normal = self.__findAllInfo(patternGroup = self.normalPatterns)
+		news = []
+		for each in normal:
+			soup = BeautifulSoup(each.group(), 'html.parser')
+			if self.source == 'metro':
+				try:
+					img = soup.li['data-thumbnail']
+				except KeyError:
+					img = soup.li.img['data-original']
+				url = soup.li.a['href']
+				title = soup.li.a.text
+				content = ''
+				try:
+					content = soup.li.img['alt']
+				except: 
+					pass
+				news.append(News(title = title, img = img, url = url, source = self.source))
+			elif self.source == 'chronicle':
+				url = soup.div.span.a['href']
+				title = soup.div.span.a.text
+				news.append(News(title = title, url = url, source = self.source))
+		return news
+
+
 	def __processChronicleHeadline(self, index):
 		introPattern = re.compile('<div id="tab-' + str(index) + '" class="ui-tabs-panel (ui-tabs-hide)?">(.|\s)*?<\/div>')
 		return re.search(introPattern, self.data).group()
 
-seeker = NewsSeeker('http://thechronicleherald.ca/', 'chronicle')
-seeker.process()
+	def __processMetroSubHeadlines(self):
+		subPattern = re.compile('<li data-vr-contentbox="">(\s|.)*?<\/li>')
+		result = re.finditer(subPattern, self.data)
+		news = []
+		for each in result:
+			soup = BeautifulSoup(each.group(), 'html.parser')
+			url = soup.a['href']
+			img = soup.img['data-original']
+			content = soup.img['alt']
+			title = soup.li.contents[len(soup.li.contents) - 2].text
+			news.append(News(title = title, url = url, img = img, content = content, source = self.source))
+		return news
