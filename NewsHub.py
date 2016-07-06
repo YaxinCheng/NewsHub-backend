12f705ec9914152cc3983e4efe9422efa3a3b1e4
WatashiJ <yx299612@dal.ca>
2016-07-06 14:36:39 -0300

    New api for all news

--- NewsHub.py
+++ NewsHub.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, abort
 from flask_pymongo import PyMongo
 from flask_restful import Resource, Api
 from bson.json_util import dumps
@@ -30,45 +30,49 @@ class index(Resource):
 	def get(self):
 		return {"Hello": "World"}
 
+class parseAllPage(Resource):
+	def get(self):
+		URLs = {'metro': 'http://www.metronews.ca/halifax.html', 'chronicle': 'http://thechronicleherald.ca/'}
+		headlines = mongo.db.headlines.find()
+		normal = mongo.db.normal.find()
+		head = []
+		norm = []
+		if headlines.count() > 0 and normal.count() > 0:
+			for each in headlines:
+				head += each['content']
+			for each in normal:
+				norm += each['content']
+			return {'headlines': head, 'normal': norm}
+		else:
+			headlines = []
+			normal = []
+			for source in URLs.keys():
+				url = URLs[source]
+				crawler = NewsSeeker(url = url, source = source)
+				news = crawler.process()
+				headlines.append(news['headlines'])
+				normal.append(news['normal'])
+				mongo.db.headlines.save({'_id': source, 'content': news['headlines']})
+				mongo.db.normal.save({'_id': source, 'content': news['normal']})
+			result = {'headlines': headlines, 'normal': normal}
+			return result
+
 class parsePage(Resource):
 	def get(self, source):
 		URLs = {'metro': 'http://www.metronews.ca/halifax.html', 'chronicle': 'http://thechronicleherald.ca/'}
-		if source == 'all':
-			headlines = mongo.db.headlines.find()
-			normal = mongo.db.normal.find()
-			head = []
-			norm = []
-			if headlines.count() > 0 and normal.count() > 0:
-				for each in headlines:
-					head += each['content']
-				for each in normal:
-					norm += each['content']
-				return {'headlines': head, 'normal': norm}
-			else:
-				headlines = []
-				normal = []
-				for source in URLs.keys():
-					url = URLs[source]
-					crawler = NewsSeeker(url = url, source = source)
-					news = crawler.process()
-					headlines.append(news['headlines'])
-					normal.append(news['normal'])
-					mongo.db.headlines.save({'_id': source, 'content': news['headlines']})
-					mongo.db.normal.save({'_id': source, 'content': news['normal']})
-				result = {'headlines': headlines, 'normal': normal}
-				return result
+		if not source in URLs:
+			abort(404)
+		headlines = mongo.db.headlines.find({'_id': source})
+		normal = mongo.db.normal.find({'_id': source})
+		if headlines.count() > 0 and normal.count() > 0:
+			return {'headlines': headlines[0]['content'], 'normal': normal[0]['content']}
 		else:
-			headlines = mongo.db.headlines.find({'_id': source})
-			normal = mongo.db.normal.find({'_id': source})
-			if headlines.count() > 0 and normal.count() > 0:
-				return {'headlines': headlines[0]['content'], 'normal': normal[0]['content']}
-			else:
-				url = URLs[source]
-				crawler = NewsSeeker(url = url, source = source)
-				result = crawler.process()
-				mongo.db.headlines.save({'_id': source, 'content': result['headlines']})
-				mongo.db.normal.save({'_id': source, 'content': result['normal']})
-				return result
+			url = URLs[source]
+			crawler = NewsSeeker(url = url, source = source)
+			result = crawler.process()
+			mongo.db.headlines.save({'_id': source, 'content': result['headlines']})
+			mongo.db.normal.save({'_id': source, 'content': result['normal']})
+			return result
 			
 class parseNews(Resource):
 	def post(self):
@@ -84,6 +88,7 @@ class parseNews(Resource):
 
 api.add_resource(index,'/')
 api.add_resource(parseNews, '/api/details')
+api.add_resource(parseAllPage, '/api/news/')
 api.add_resource(parsePage,'/api/news/<string:source>')
 if __name__ == '__main__':
 	app.run()
