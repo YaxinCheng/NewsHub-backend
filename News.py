4c05da47d6347177a35f8c324c7ba1f971d321d2
WatashiJ <yx299612@dal.ca>
2016-07-12 13:01:58 -0300

    News tags added, and sort with tags are enabled

--- News.py
+++ News.py
@@ -6,6 +6,7 @@ class News:
 		self.date = date
 		self.content = content
 		self.img = img
+		self.tag = self.__tag()
 
 	def toDict(self):
 		emptyDict = dict()
@@ -14,9 +15,28 @@ class News:
 		emptyDict['_id'] = self.url
 		emptyDict['content'] = self.content
 		emptyDict['img'] = self.img
+		emptyDict['tag'] = self.tag
 		if len(self.date) > 0:
 			emptyDict['date'] = self.date
 		return emptyDict
 
 	def __str__(self):
 		return 'Title: ' + self.title + '\nSource: ' + self.source + '\nURL: ' + self.url + '\nDate: ' + self.date + '\nContent: ' + self.content + '\nImg: ' + self.img
+
+	def __tag(self):
+		components = self.url.split('/')
+		filterWords = ['news', 'views', 'content']
+		for index in range(3, len(components)):
+			if not components[index] in filterWords:
+				return self.__formatTag(components[index])
+
+	def __formatTag(self, tag):
+		replaceWords = {'novascotia': 'halifax', 'halifax': 'halifax', 
+						'bluejays': 'sports', 'sports': 'sports',
+						'artslife': 'life', 'homesnews': 'life', 'life': 'life', 
+						'wheelnews': 'wheels', 'metro': 'other', 'chronicle': 'other'}
+		if tag.isdigit():
+			return 'other'
+		if tag in replaceWords:
+			return replaceWords[tag]
+		return tag
