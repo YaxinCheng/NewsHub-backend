7876f50d932caa5d4eb23f299f1ea6564c5d552b
WatashiJ <yx299612@dal.ca>
2016-07-15 10:41:51 -0300

    Change the news refresh time

--- NewsRefresher.py
+++ NewsRefresher.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 URLs = {'metro': 'http://www.metronews.ca/halifax.html', 'chronicle': 'http://thechronicleherald.ca/'}
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', hours = 0.5)
+@sched.scheduled_job('interval', hours = 2)
 def refresh_news():
 	client = MongoClient('mongodb://heroku_gfp8zr4k:mu22sv8pm9q3b5o286vfjjq870@ds015335.mlab.com:15335/heroku_gfp8zr4k')
 	db = client.heroku_gfp8zr4k
@@ -36,13 +36,5 @@ def remove_news_caches():
 	db = client.heroku_gfp8zr4k
 	db.details.drop()
 	client.close()
-
-@sched.scheduled_job('cron', day_of_week = 'mon-sun', hour = 22)
-def remove_everything():
-	client = MongoClient('mongodb://heroku_gfp8zr4k:mu22sv8pm9q3b5o286vfjjq870@ds015335.mlab.com:15335/heroku_gfp8zr4k')
-	db = client.heroku_gfp8zr4k
-	db.headlines.delete_many({})
-	db.images.delete_many({})
-	db.normal.delete_many({})
 
 sched.start()
